6f774fd6df91cbadd0b1531917dbc014fdc10a3b
dshubenok <dshubenok93@gmail.com>
2020-09-10 00:26:02 +0300

    fdf

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello my dear friends friends1f!'
+    return 'hello my dear friends friends1f111!!!!!!'
 
 
 if __name__ == '__main__':

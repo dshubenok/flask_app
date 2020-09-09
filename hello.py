351a08b2706d52414bee1a63a7ba597f06bec461
dshubenok <dshubenok93@gmail.com>
2020-09-09 21:24:49 +0300

    fdf

--- hello.py
+++ hello.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'hello my dear friends friends1!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
